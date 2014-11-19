b47739f162e07a622a39f8f1b3ba6d8474ed371c
Borja Morales <borja.morales@softonic.com>
2014-11-19 15:04:20 +0100

    First commit

---
+++ src/Memcached.php
@@ -0,0 +1,59 @@
+<?php
+namespace Kizilare\Cache\Memcached;
+
+class Memcached
+{
+    /**
+     * Determines if the actions must override the previous ones.
+     *
+     * @var boolean
+     */
+    public $override = false;
+
+    /**
+     * Memcached server.
+     *
+     * @var Memcached
+     */
+    protected $connection = null;
+
+    /**
+     * Thread id.
+     *
+     * @var string
+     */
+    protected $thread_id;
+
+    public function __construct( $thread_id )
+    {
+        $this->thread_id = $thread_id;
+        $this->connection = new \Memcached();
+        $servers = \Kizilare\Framework\App::getInstance()->getConfig( 'memcached' );
+        $this->connection->addServers( $servers );
+    }
+
+    /**
+     * loads the script information.
+     *
+     * @param string $key
+     *
+     * @return array
+     */
+    public function get( $key )
+    {
+        return $this->connection->get( $key );
+    }
+
+    /**
+     * Saves date into file.
+     *
+     * @param string $key
+     * @param        $data
+     */
+    public function set( $key, $data )
+    {
+        $this->connection->set( $key, $data );
+    }
+}
+
+?>
